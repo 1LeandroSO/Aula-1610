b256678de57aacaec99580f229b5346ab93fbf95
gustavo17042 <130669211+gustavo17042@users.noreply.github.com>
2023-10-16 09:50:24 -0300

    Primeiro Comitt

---
+++ Leandro Olavo - Aula-1 e 2 - Curso de Pyton - Manhã/Desafios/Aula 2 da Pasta Desafio/par_impar.py
@@ -0,0 +1,16 @@
+# Sistema que verifica se um número infor
+
+# Apresentação do Sistema
+
+print('\n\t\t\t -- Verificador de Num. Pa')
+
+# Entrada
+num = int(input('Informe um número>: '))
+
+# Processamento e Saída
+
+if num % 2 == 0:
+    print(f'{num} é par!')
+else:
+    print(f'{num} é ímpar!')
+

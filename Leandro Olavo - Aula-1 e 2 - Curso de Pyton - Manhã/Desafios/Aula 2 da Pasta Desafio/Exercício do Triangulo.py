6ab92a6113a39fe4786387051b775602adc6d914
gustavo17042 <130669211+gustavo17042@users.noreply.github.com>
2023-10-16 12:00:39 -0300

    Triângulo Comitt Leandro

---
+++ Leandro Olavo - Aula-1 e 2 - Curso de Pyton - Manhã/Desafios/Aula 2 da Pasta Desafio/Exercício do Triangulo.py
@@ -0,0 +1,18 @@
+# Informa o tipo do triângulo que existe
+
+# Apresentação
+
+# Informa o tipo do triângulo que existe
+
+print('\n\t\t\t -- Validor de Triângulo -- \n\n')
+
+# Entradas
+l1 = int(input('Informe o lado a: '))
+l2 = int(input('Informe o lado b: '))
+l3 = int(input('Informe o lado c: '))
+
+# Processamento e Saída
+if l1 < (l2 + l3) and l2 < (l1 + l3) and l3 < (l1 + l2):
+    print(f'\n{l1}, {l2} e {l3} formam triângulo! ')
+else:
+    print(f'\n{l1}, {l2} e {l3} não formam triângulo.')
